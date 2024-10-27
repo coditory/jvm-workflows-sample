45cc6b22d9d7414a4c3e1ade51ddbcfeb5562dd9
ogesaku <ogesaku@gmail.com>
2024-10-27 10:43:49 +0100

    Test dependabot

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     val coroutinesVersion = "1.9.0"
-    implementation("com.coditory.klog:klog:0.0.17")
+    implementation("com.coditory.klog:klog:0.0.16")
     implementation("com.coditory.quark:quark-context:0.1.22")
 
     // unit tests
